7caf9222ef26f672b81248f61bc65069be398bd2
thehongbaokhanh <thehongbaokhanh2005@gmail.com>
2024-12-04 16:02:39 +0700

    car management

---
+++ src/main/java/com/example/demo/OOP/car.java
@@ -0,0 +1,57 @@
+package com.example.demo.OOP;
+
+public class Car {
+    String name;
+    String manufacturer;
+    double price;
+    int status;
+
+    public Car(String name, String manufacturer, double price, int status) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.price = price;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", price=" + price +
+                ", status=" + status +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
